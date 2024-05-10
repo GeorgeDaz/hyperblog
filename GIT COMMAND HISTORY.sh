9ba7daf1f5610a9ad0b0a5b9af269fa96c99777f
GeorgeDaz <jorgedazavega@gmail.com>
2024-05-10 17:24:28 -0500

    imagen capsula agregada

---
+++ GIT COMMAND HISTORY.sh
@@ -0,0 +1,274 @@
+  GIT COMMAND HISTORY
+  
+  
+    1  git --version
+    2  git clone https://github.com/GeorgeDaz/Prep-Course.git
+    3  git config --get-all user.name
+    4  git init
+    5  subl .
+    6  version --node
+    7  node --version
+    8  dir
+    9  ls
+   10  pwd
+   11  pwd
+   12  cd /
+   13  ls
+   14  ls -al
+   15  ls -a
+   16  ls -l
+   17  clear
+   18  cd /c
+   19  ls
+   20   cd users
+   21  cd ..
+   22  cd Users
+   23  cd..
+   24  cd . .
+   25  cd ..
+   26  clear
+   27  ls
+   28  cd ..
+   29  ls
+   30  ls
+   31  cd ..
+   32  cd
+   33  pwd
+   34  ls
+   35  mkdir proyecto1
+   36  ls
+   37  pwd
+   38  cd proyecto1
+   39   touch vacio.txt
+   40  ls
+   41  pwd
+   42  cd ..
+   43  cd proyecto1
+   44  ls
+   45  cat vacio.txt
+   46  cat vacio.txt
+   47  history
+   48  ls
+   49  history
+   50  rm vacio.txt
+   51  ls
+   52  rm --help
+   53  ls
+   54  pwd
+   55  git init
+   56  ls -al
+   57  code
+   58  code
+   59  cat prueba.txt
+   60  gti status
+   61  git status
+   62  git add .
+   63  git status
+   64  git rm prueba.txt
+   65  git rm --cached prueba.txt
+   66  git status
+   67  git add prueba.txt
+   68  git status
+   69  git commit -m 'first'
+   70  git config
+   71  git config --list
+   72  pwd
+   73  ls
+   74  cd proyecto1
+   75  ls}
+   76  ls
+   77  git config --list
+   78  pwd
+   79  ls
+   80  cd proyecto1/
+   81  git status
+   82  git status
+   83  git add .
+   84  git status
+   85  git commit -m 'Segundo commit'
+   86  git status
+   87  git log prueba.txt
+   88  history
+   89  cat prueba.txt
+   90  git show prueba.txt
+   91  pwd
+   92  ls
+   93  cd proyecto1/
+   94  ls
+   95  git status
+   96  code
+   97  pwd
+   98  ls/
+   99  ls
+  100  cd proyecto1/
+  101  code
+  102  git status
+  103  git status
+  104  git log
+  105  git show
+  106  git checkout master
+  107  git add .
+  108  git commit -m 'Nueva version con parrafo nuevo'
+  109  git status
+  110  git checkout cabecera
+  111  git checkout master
+  112  git branch
+  113  git merge cabecera
+  114  git branch
+  115  git log
+  116  git add .
+  117  git commit -m 'quinto commit'
+  118  git status
+  119  git show
+  120  git log
+  121  git checkout cabecera
+  122  git merge master
+  123  git add .
+  124  git commit -m 'sexto commit'
+  125  git checkout master
+  126  git add .
+  127  git commit -m 'agregados nuevos elementos'
+  128  git merge cabecera
+  129  git merge cabecera
+  130  gti status
+  131  git status
+  132  git add .
+  133  git commit -m 'septimo commit'
+  134  git log
+  135  git checkout cabecera
+  136  git checkout master
+  137  git remote add origin https://github.com/GeorgeDaz/hyperblog.git
+  138  git remote
+  139  git remote -v
+  140  ls
+  141  ls -al
+  142  git push origin master
+  143  history
+  144  git remote
+  145  git remote -v
+  146  git status
+  147  git pull origin master
+  148  git pull origin main
+  149  git pull origin master --allow-unrelated-histories
+  150  git pull origin main --allow-unrelated-histories
+  151  git branch
+  152  ls -al
+  153  git status
+  154  git push origin master
+  155  git status
+  156  git pull origin main
+  157  git status
+  158  git status
+  159  git add .
+  160  git commit -m 'correccion hecha del titulo'
+  161  git push origin main
+  162  git push origin master
+  163  git remote -v
+  164  git push origin main
+  165  git status
+  166  git add .
+  167  git commit -m 'subtitulo actualizado'
+  168  git push origin main
+  169  git push origin master
+  170  git pull origin master
+  171  git push origin master
+  172  git log
+  173  git branch
+  174  git branch
+  175  git origin
+  176  git remote -v
+  177  git remote -v
+  178  git branch
+  179  pwd
+  180  ls -al
+  181  ls
+  182  cd
+  183  ls
+  184  pwd
+  185  git config -l
+  186  git config --global user.email ¨jorgedazavega@gmail.com¨
+  187  git config -l
+  188  ssh-keygen -t rsa -b 4096 -C ¨jorgedazavega@gmail.com¨
+  189  eval $(ssh-agent -s)
+  190  ~
+  191  cd /
+  192  pwd
+  193  cd ~
+  194  pwd
+  195  ls
+  196  cd ~/proyecto1/
+  197  cd ~/.ssh/
+  198  ls
+  199  ls -al
+  200  cd
+  201  pwd
+  202  ssh-add ~/.ssh/id_rsa
+  203   pwd
+  204  cd proyecto1/
+  205  git remote -v
+  206  git remote
+  207  git remore set-url origin git@github.com:GeorgeDaz/hyperblog.git
+  208  git remote set-url origin git@github.com:GeorgeDaz/hyperblog.git
+  209  git remote -v
+  210  gti status
+  211  git status
+  212  git add .
+  213  git commit -m 'SSH conectado'
+  214  git status
+  215  git pull
+  216  git pull origin master
+  217  git status
+  218  git status
+  219  git diff
+  220  git add .
+  221  git commit -m 'SSH coenctado 2.0'
+  222  git status
+  223  git log
+  224  git pull origin master
+  225  git push origin master
+  226  git pull origin master
+  227  git push origin master
+  228  git log
+  229  git log --all
+  230  git log --all --graph
+  231  git log --all --graph --decorate --oneline
+  232  alias arbolito=¨git log --all --graph --decorate --oneline¨
+  233  arbolito
+  234  alias arbolito='git log --all --graph --decorate --oneline'
+  235  arbolito
+  236  git tag -a v0.1 -m 'Resultado primeras clases' bb22b5f
+  237  git tags
+  238  git tag
+  239  git show-ref --tags
+  240  git pull origin master
+  241  git push origin --tags
+  242  git tag -a v2.0 -m 'titulo agregado' 4e4dd21
+  243  git tag
+  244  git pull origin master
+  245  git push origin --tags
+  246  git tag -d v2.0
+  247  git tag
+  248  git pull origin master
+  249  git push origin --tags
+  250  git tag
+  251  git push origin :refs/tags/v2.0
+  252  git branch
+  253  git branch
+  254  git checkout cabecera
+  255  git checkout master
+  256  git show-branch
+  257  git show-branch --all
+  258  gitk
+  259  git branch
+  260  git pull origin master
+  261  git checkout cabecera
+  262  git push origin cabecera
+  263  git checkout master
+  264  git branch header
+  265  git branch footer
+  266  git branch
+  267  git push origin header
+  268  git push origin footer
+  269  git hsitory
+  270  git history
+  271  history
